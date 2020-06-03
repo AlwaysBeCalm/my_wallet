975586a18ea1a4a005b20bf8ba37ff488b820e46
AlwaysBeCalm <AlwaysBeCalm@users.noreply.github.com>
2020-06-03 21:12:59 +0300

    script for Linux and MAC to install the required packages

--- run.sh
+++ run.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-function normal()
-{
+loc=$PWD
 
 python3 -m venv $loc/.venv
 
@@ -18,18 +17,3 @@ $loc/.venv/bin/pip3 install -r $loc/requirements.txt
 $loc/.venv/bin/pip3.7 install -r $loc/requirements.txt
 
 $loc/.venv/bin/python3 $loc/src/main.py
-}
-
-loc=$PWD
-
-echo """welcome to venv creator script
-this script will create a .venv folder for the app 'my_wallet' to be able to run.
-press [enter] to create or type 'q' to cancel"""
-echo -n ''
-read option
-
-if [ -z "$option" ]; then
-	normal;
-elif [ "$option" == "q" ]; then
-	exit 0;
-fi;
