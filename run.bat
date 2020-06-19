ce99866991f94201284a1faccc5e872733bcdb2f
AlwaysBeCalm <AlwaysBeCalm@users.noreply.github.com>
2020-06-19 17:23:22 +0300

    for windows
    a script for windows that will download the required libraries for the app to run

--- run.bat
+++ run.bat
@@ -2,11 +2,12 @@ set pth=%cd%
 
 python3 -m venv %pth%\.venv
 
-%pth%\.venv\Scripts\python.exe -m pip install --upgrade pip
+%pth%\.venv\Scripts\python*.exe -m pip install --upgrade pip
+%pth%\.venv\Scripts\python*.exe -m pip install --upgrade pip3
 
-%pth%\.venv\Scripts\pip.exe install -r requirements.txt
+%pth%\.venv\Scripts\pip.exe install -r %pth%\requirements.txt
 
-%pth%\.venv\Scripts\pip3.exe install -r requirements.txt
+%pth%\.venv\Scripts\pip3.exe install -r %pth%\requirements.txt
 
 %pth%\.venv\Scripts\python.exe %pth%\src\main.py
 
